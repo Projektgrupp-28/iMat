fb8043aac3af68e76590649a78ce01c88e735def
williamProgrammerar <70644042+williamProgrammerar@users.noreply.github.com>
2021-05-06 13:38:09 +0200

    Init

---
+++ src/iMat.java
@@ -0,0 +1,14 @@
+import se.chalmers.cse.dat216.project.IMatDataHandler;
+import se.chalmers.cse.dat216.project.Product;
+
+public class iMat {
+
+    public static void main(String[] args) {
+        IMatDataHandler dataHandler = IMatDataHandler.getInstance();
+        Product product = dataHandler.getProduct(2);
+        System.out.println(product.getName() + " kostar " + (int)product.getPrice() + "kr");
+
+
+        System.out.println(dataHandler.getCustomer().getFirstName());
+    }
+}
---
+++ src/iMatBackendController.java
@@ -0,0 +1,2 @@
+public class iMatBackendController {
+}
