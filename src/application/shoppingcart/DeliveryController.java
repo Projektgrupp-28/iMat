b5c7a42dd3723995298936d0a8220ea8ca013f88
Philip Winsnes <philip.winsnes@icloud.com>
2021-05-27 19:58:58 +0200

    Fixed error handling in cvc and zipcode

--- src/application/shoppingcart/DeliveryController.java
+++ src/application/shoppingcart/DeliveryController.java
@@ -4,6 +4,8 @@ import application.Model;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Border;
+import javafx.scene.paint.Color;
 import se.chalmers.cse.dat216.project.Customer;
 
 import java.net.URL;
@@ -30,6 +32,30 @@ public class DeliveryController implements Initializable {
         customer.setLastName(city.getText());
         customer.setPhoneNumber(telephone.getText());
         customer.setPostAddress(zipcode.getText());
+    }
+
+    @FXML
+    private void zipCodeTypeCheck() {
+        if (zipcode.getText().length() > 5) {
+            zipcode.deletePreviousChar();
+            // TODO: Jump to next.
+        } else if (!zipcode.getText().matches("\\d+")) {
+            // Given text does not include digits.
+            zipcode.deletePreviousChar();
+        } else if (zipcode.getText().length() == 5) {
+            System.out.println("Complete!");
+            zipcode.setStyle("-fx-border-color: green");
+        } else {
+            System.out.println("Incomplete");
+            zipcode.setStyle("-fx-border-color: red");
+        }
+    }
+
+    @FXML
+    private void nameLabelTypeCheck() {
+        if (zipcode.getText().matches("[abc]")) {
+            System.out.println("Hello");
+        }
     }
 
     private void prepopulateFields (Customer customer) {
